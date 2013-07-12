1b87600c47bbbcfba9b90464dc765c90fc24d65b
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-07-12 17:18:48 +0200

    add a test

---
+++ main_test.rb
@@ -0,0 +1,30 @@
+require './main'
+require 'test/unit'
+require 'rack/test'
+
+set :environment, :test
+
+class MainTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_root_returns_home
+    get '/'
+    assert last_response.ok?
+    # assert page.has_content?('Welcome')
+    # assert_equal 'Welcome', 
+    # assert_equal 'Hello World!', last_response.body
+  end
+
+  def test_song_adds_new_song_object
+    get '/songs/new'
+    assert last_response.ok?
+    #
+    # assert page.has_content?('Welcome')
+    # assert_equal 'Welcome', 
+    # assert_equal 'Hello World!', last_response.body
+  end
+end
